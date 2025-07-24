e654e6cc778c308c6dffe33eea846b90ec355550
wOwOv <regret6458@gmail.com>
2025-07-24 17:50:02 +0900

    Add files via upload

--- TlsMemoryPool.h
+++ TlsMemoryPool.h
@@ -2,12 +2,31 @@
 #define  __TLSMEMORYPOOL__
 #include <new.h>
 #include <windows.h>
-#include "BucketStack.h"
+
+static unsigned long Cookie = 0x01010100;
+
+#define MAX 5000000
+unsigned long long Tlslogindex = 0;
+struct TlsLogBox
+{
+	DWORD id;
+	int type = 0xcccccccc;//0xbbbbbbbb Enqueue 0xdddddddd Dequeue
+	int size = 0xcccccccc;
+	int cookie = 0xcccccccc;
+	void* curtail = nullptr;
+	void* oldtail = nullptr;
+	void* newtail = nullptr;
+};
+
+TlsLogBox Tlslogbox[MAX];
 
 template <class DATA>
 class TlsMemoryPool
 {
 private:
+	const unsigned long long ADRMASK = 0x0000ffffffffffff;
+	const unsigned long long TAGMASK = 0xffff000000000000;
+	const unsigned long long MAKETAG = 0x0001000000000000;
 
 	struct Node
 	{
@@ -15,7 +34,9 @@ private:
 		DATA _data;
 		int _overguard;
 		Node* _next;
-	};
+		Node* _bunchnext;
+	}typedef Bunch;
+
 	struct TlsPool
 	{
 		Node* _nodelist;
@@ -29,7 +50,7 @@ private:
 
 public:
 
-	TlsMemoryPool(int BlockNum=100, int bunchsize=100,bool PlacementNew = false, bool maxflag = false)
+	TlsMemoryPool(int BlockNum = 100, int bunchsize = 100, bool PlacementNew = false, bool maxflag = false)
 	{
 		_tlsIndex = TlsAlloc();
 		if (_tlsIndex == TLS_OUT_OF_INDEXES)
@@ -37,19 +58,27 @@ public:
 			DebugBreak();
 		}
 
-		_cookie = _bucketstack.GetCommoncookie();
-	
+		_cookie = InterlockedIncrement(&Cookie);
+
 		Node* node = new Node;
 		_position = (long long)&node->_data - (long long)&node->_underguard;
 		delete node;
 
 		_pnFlag = PlacementNew;
-		_maxcount = BlockNum;
+		_maxCount = BlockNum;
 		_maxFlag = maxflag;
 
-		_bunchsize = bunchsize;
+		_bunchSize = bunchsize;
 
+		_usingCount = 0;
 
+		//더미 bunch 생성
+
+		_dummy = new Bunch;
+		_dummy->_bunchnext = nullptr;
+		_head = _dummy;
+		_tail = _dummy;
+		_bunchCount = 0;
 
 	}
 	~TlsMemoryPool()
@@ -62,20 +91,20 @@ public:
 
 		Node* node = tpool->_nodelist;
 		Node* temp = node->_next;
-			while (1)
+		while (1)
+		{
+			if (node != nullptr)
 			{
-				if (node != nullptr)
-				{
-					temp = node->_next;
-					delete node;
-					node = temp;
-				}
-				else
-				{
-					break;
-				}
+				temp = node->_next;
+				delete node;
+				node = temp;
 			}
-		
+			else
+			{
+				break;
+			}
+		}
+
 
 		node = tpool->_freelist;
 		temp = node->_next;
@@ -113,7 +142,7 @@ public:
 			//생성자 호출한 상태로 들어가야함
 			if (_pnFlag == 0)
 			{
-				for (int i = 0; i < _maxcount; i++)
+				for (int i = 0; i < _maxCount; i++)
 				{
 					Node* node = new Node;
 					node->_underguard = _cookie;
@@ -125,7 +154,7 @@ public:
 			}
 			else//생성자 호출 없이 들어가야함
 			{
-				for (int i = 0; i < _maxcount; i++)
+				for (int i = 0; i < _maxCount; i++)
 				{
 					Node* node = (Node*)malloc(sizeof(Node));
 					node->_underguard = _cookie;
@@ -136,8 +165,8 @@ public:
 				}
 			}
 
-			tpool->_nodeCount = _maxcount;
-			tpool->_storedCount = tpool->_nodeCount+tpool->_freeCount;
+			tpool->_nodeCount = _maxCount;
+			tpool->_storedCount = tpool->_nodeCount + tpool->_freeCount;
 
 			TlsSetValue(_tlsIndex, (LPVOID)tpool);
 		}
@@ -153,7 +182,6 @@ public:
 				new(&(allocated->_data)) DATA;
 			}
 			tpool->_nodeCount--;
-			tpool->_storedCount--;
 		}
 		//내 노드는 없고 반환하려고 보관해둔 노드가 있다면
 		else if (tpool->_freeCount > 0)
@@ -161,17 +189,16 @@ public:
 			allocated = tpool->_freelist;
 			tpool->_freelist = allocated->_next;
 			//_pnFlag가 1이면 생성자 호출해서 나가야함
-			if ( _pnFlag != 0)
+			if (_pnFlag != 0)
 			{
 				new(&(allocated->_data)) DATA;
 			}
 			tpool->_freeCount--;
-			tpool->_storedCount--;
 		}
 		//공용풀에서 받아와야한다면
 		else
 		{
-			Node* nodebunch = (Node*)_bucketstack.GetBucket();
+			Node* nodebunch = GetBucket();
 			//공용풀에서 노드묶음 받아옴
 			if (nodebunch != nullptr)
 			{
@@ -182,23 +209,25 @@ public:
 				allocated = tpool->_nodelist;
 				tpool->_nodelist = allocated->_next;
 				//_pnFlag가 1이면 생성자 호출해서 나가야함
-				if ( _pnFlag != 0)
+				if (_pnFlag != 0)
 				{
 					new(&(allocated->_data)) DATA;
 				}
-				tpool->_nodeCount =  _bunchsize - 1;
-				tpool->_storedCount = tpool->_nodeCount + tpool->_freeCount;
+				tpool->_nodeCount = _bunchSize - 1;
 			}
 			//공용풀에서도 못 받아왔음
 			else
 			{
 				//진짜 할당해서 줘야함
 				allocated = new Node;
-				allocated->_underguard =  _cookie;
+				allocated->_underguard = _cookie;
 				allocated->_overguard = _cookie;
 			}
-			
+
 		}
+		tpool->_storedCount = tpool->_nodeCount + tpool->_freeCount;
+
+		InterlockedIncrement(&_usingCount);
 
 		return &(allocated->_data);
 	}
@@ -216,11 +245,11 @@ public:
 	{
 		//노드할당 주소 찾기
 		char* address = (char*)data;
-		address -=  _position;
+		address -= _position;
 		Node* retnode = (Node*)address;
 
 		//언더.오버플로우 확인 겸 맞는 요소가 들어왔는지 확인
-		if (retnode->_underguard !=  _cookie || retnode->_overguard !=  _cookie)
+		if (retnode->_underguard != _cookie || retnode->_overguard != _cookie)
 		{
 			return false;
 		}
@@ -238,7 +267,7 @@ public:
 			//생성자 호출한 상태로 들어가야함
 			if (_pnFlag == 0)
 			{
-				for (int i = 0; i < _maxcount; i++)
+				for (int i = 0; i < _maxCount; i++)
 				{
 					Node* node = new Node;
 					node->_underguard = _cookie;
@@ -250,7 +279,7 @@ public:
 			}
 			else//생성자 호출 없이 들어가야함
 			{
-				for (int i = 0; i < _maxcount; i++)
+				for (int i = 0; i < _maxCount; i++)
 				{
 					Node* node = (Node*)malloc(sizeof(Node));
 					node->_underguard = _cookie;
@@ -261,25 +290,24 @@ public:
 				}
 			}
 
-			tpool->_nodeCount = _maxcount;
-			tpool->_storedCount = _maxcount;
+			tpool->_nodeCount = _maxCount;
+			tpool->_storedCount = tpool->_nodeCount + tpool->_freeCount;
 
 			TlsSetValue(_tlsIndex, (LPVOID)tpool);
 		}
 
 		//_pnFlag가 1이라면 소멸자 호출해서 보관
-		if ( _pnFlag != 0)
+		if (_pnFlag != 0)
 		{
 			retnode->_data.~DATA();
 		}
 
 		//nodelist 자리가 있다면
-		if (tpool->_nodeCount <  _bunchsize)
+		if (tpool->_nodeCount < _bunchSize)
 		{
 			retnode->_next = tpool->_nodelist;
 			tpool->_nodelist = retnode;
 			tpool->_nodeCount++;
-			tpool->_storedCount++;
 		}
 		//nodelist에 자리가 없다면 freelist로
 		else
@@ -287,17 +315,18 @@ public:
 			retnode->_next = tpool->_freelist;
 			tpool->_freelist = retnode;
 			tpool->_freeCount++;
-			tpool->_storedCount++;
 
 			//freelist가 다 찼다면
-			if (tpool->_freeCount ==  _bunchsize)
+			if (tpool->_freeCount == _bunchSize)
 			{
-				_bucketstack.ReturnBucket(tpool->_freelist);
+				ReturnBucket(tpool->_freelist);
 				tpool->_freelist = nullptr;
 				tpool->_freeCount = 0;
 			}
 		}
 		tpool->_storedCount = tpool->_nodeCount + tpool->_freeCount;
+
+		InterlockedDecrement(&_usingCount);
 
 		return true;
 	}
@@ -309,7 +338,7 @@ public:
 	// Parameters: 없음.
 	// Return: (int) 메모리 풀 내부 전체 개수
 	//////////////////////////////////////////////////////////////////////////
-	int	GetCapacityCount(void)
+	int	GetStoredNodeCount(void)
 	{
 		TlsPool* tpool = (TlsPool*)TlsGetValue(_tlsIndex);
 		if (tpool == nullptr)
@@ -319,26 +348,150 @@ public:
 		return tpool->_storedCount;
 	}
 
+	unsigned long long GetBunchCount()
+	{
+		return _bunchCount;
+	}
+
+	unsigned long GetUsingCount()
+	{
+		return _usingCount;
+	}
+
+	//Bucket 스택에서 얻어보고 반환할 때 쓰는 함수
+private:
+	void ReturnBucket(Node* nodebunch)
+	{
+		nodebunch->_bunchnext = nullptr;
+		unsigned long long tagbunch = (unsigned long long)nodebunch;
+
+		Bunch* oldtail;
+		Bunch* realadr;
+		do
+		{
+			while (1)
+			{
+				oldtail = _tail;
+				unsigned long long tag = (unsigned long long) oldtail;
+
+				tag &= TAGMASK;
+				tag += MAKETAG;
+				tagbunch |= tag;
+
+				unsigned long long tempadr = (unsigned long long)oldtail;
+				tempadr &= ADRMASK;
+				realadr = (Bunch*)tempadr;
+				if (realadr->_bunchnext != nullptr)
+				{
+					InterlockedCompareExchange64((__int64*)& _tail, (__int64)realadr->_bunchnext, (__int64)oldtail);
+				}
+				else
+				{
+					break;
+				}
+			}
+		} while (InterlockedCompareExchange64((__int64*)&realadr->_bunchnext, (__int64)tagbunch, (__int64)nullptr) != (__int64)nullptr);
+		int qsize=InterlockedIncrement(&_bunchCount);
+		__int64 curtail=InterlockedCompareExchange64((__int64*)&_tail, (__int64)tagbunch, (__int64)oldtail);
+
+
+		unsigned long long index = InterlockedIncrement(&Tlslogindex);
+		index %= MAX;
+		Tlslogbox[index].id = GetCurrentThreadId();
+		Tlslogbox[index].type = 0xbbbbbbbb;
+		Tlslogbox[index].size = qsize;
+		Tlslogbox[index].curtail = (Node*)curtail;
+		Tlslogbox[index].oldtail = (Node*)oldtail;
+		Tlslogbox[index].newtail = (Node*)tagbunch;
+
+	}
+	Node* GetBucket()
+	{
+		Bunch* oldhead;
+		Bunch* newhead;
+		Bunch* realadr;
+		unsigned long long tempadr;
+		unsigned long long tempadr2;
+
+		Node* retptr;
+
+		long size = InterlockedDecrement(&_bunchCount);
+		if (size < 0)
+		{
+			InterlockedIncrement(&_bunchCount);
+			return nullptr;
+		}
+
+		do
+		{
+			while (1)
+			{
+				Bunch* oldtail;
+				Bunch* tailadr;
+
+				oldtail = _tail;
+				unsigned long long tempadr = (unsigned long long)oldtail;
+				tempadr &= ADRMASK;
+				tailadr = (Bunch*)tempadr;
+				if (tailadr->_bunchnext == nullptr)
+				{
+					break;
+				}
+				InterlockedCompareExchange64((__int64*)&_tail, (__int64)tailadr->_bunchnext, (__int64)oldtail);
+			}
+
+			do
+			{
+				oldhead = _head;
+				tempadr = (unsigned long long)oldhead;
+				tempadr &= ADRMASK;
+				realadr = (Bunch*)tempadr;
+				newhead = realadr->_bunchnext;
+			} while (newhead == nullptr);
+
+			tempadr2 = (unsigned long long)newhead;
+			tempadr2 &= ADRMASK;
+			retptr = (Bunch*)tempadr2;
+		} while (InterlockedCompareExchange64((__int64*)&_head, (__int64)newhead, (__int64)oldhead) != (__int64)oldhead);
+
+
+		unsigned long long index = InterlockedIncrement(&Tlslogindex);
+		index %= MAX;
+		Tlslogbox[index].id = GetCurrentThreadId();
+		Tlslogbox[index].type = 0xdddddddd;
+		Tlslogbox[index].size = size;
+		Tlslogbox[index].curtail = 0x0000000000000000;
+		Tlslogbox[index].oldtail = (Node*)oldhead;
+		Tlslogbox[index].newtail = (Node*)newhead;
+
+
+
+
+
+		return retptr;
+	}
 
 private:
-
-
 	int _cookie;
 	long long _position;
 
 	bool _pnFlag;
-	int _maxcount;
+	int _maxCount;
 	bool _maxFlag;
 
-	unsigned int _bunchsize;
-	
-	DWORD _tlsIndex=0;
+	unsigned int _bunchSize;
 
-	BucketStack _bucketstack;				//static 노드버킷 관리 스택
+	unsigned long _usingCount;
+
+	DWORD _tlsIndex = 0;
+
+	Bunch* _head;
+	Bunch* _tail;
+	Bunch* _dummy;
+	unsigned long _bunchCount;				//보관 중인 Bunch개수
 };
 
 
 
 
 #endif
-
---
+++ hw_48.cpp
@@ -0,0 +1,224 @@
+﻿#include <iostream>
+#include <process.h>
+#include "LockFreeQueue.h"
+#include "CrashDump.h"
+
+#define REPEAT 10
+CrashDump dumpit;
+
+struct TestDATA
+{
+    int data;
+    unsigned long count = 0;
+};
+
+
+
+
+unsigned __stdcall WorkerThread0(LPVOID arg);
+unsigned __stdcall WorkerThread1(LPVOID arg);
+unsigned __stdcall WorkerThread2(LPVOID arg);
+unsigned __stdcall WorkerThread3(LPVOID arg);
+unsigned __stdcall WorkerThread4(LPVOID arg);
+unsigned __stdcall WorkerThread5(LPVOID arg);
+
+extern unsigned long long logindex;
+
+int main()
+{
+    LFQueue<TestDATA*> lfQ;
+    LFQueue<TestDATA*> lfQ2;
+    LFQueue<TestDATA*> lfQ3;
+
+    HANDLE hThread[12];
+    unsigned long long oldlogindex=10;
+    for (int i = 0; i < 2; i++)
+    {
+        hThread[i] = (HANDLE)_beginthreadex(NULL, 0, WorkerThread0, &lfQ, 0, NULL);
+        printf("thread%d start\n", i);
+    }
+    for (int i = 2; i < 4; i++)
+    {
+        hThread[i] = (HANDLE)_beginthreadex(NULL, 0, WorkerThread1, &lfQ, 0, NULL);
+        printf("thread%d start\n", i);
+    }
+    for (int i = 4; i < 6; i++)
+    {
+        hThread[i] = (HANDLE)_beginthreadex(NULL, 0, WorkerThread2, &lfQ2, 0, NULL);
+        printf("thread%d start\n", i);
+    }
+    for (int i = 6; i < 8; i++)
+    {
+        hThread[i] = (HANDLE)_beginthreadex(NULL, 0, WorkerThread3, &lfQ2, 0, NULL);
+        printf("thread%d start\n", i);
+    }
+    for (int i = 8; i < 10; i++)
+    {
+        hThread[i] = (HANDLE)_beginthreadex(NULL, 0, WorkerThread4, &lfQ3, 0, NULL);
+        printf("thread%d start\n", i);
+    }
+    for (int i = 10; i < 12; i++)
+    {
+        hThread[i] = (HANDLE)_beginthreadex(NULL, 0, WorkerThread5, &lfQ3, 0, NULL);
+        printf("thread%d start\n", i);
+    }
+    while (1)
+    {
+        printf("%lld \n", logindex);
+        if (oldlogindex == logindex)
+        {
+            //DebugBreak();
+        }
+        oldlogindex = logindex;
+        Sleep(3000);
+    }
+}
+
+unsigned __stdcall WorkerThread0(LPVOID arg)
+{
+    LFQueue<TestDATA*>* lfQ = (LFQueue<TestDATA*>*)arg;
+    DWORD id = GetCurrentThreadId();
+    int num = 0;
+    while (1)
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            TestDATA* data = new TestDATA;
+            data->data = num++;
+            data->count = 1;
+            lfQ->Enqueue(data);
+        }
+       Sleep(1);
+    }
+    return 0;
+}
+
+unsigned __stdcall WorkerThread1(LPVOID arg)
+{
+    LFQueue<TestDATA*>* lfQ = (LFQueue<TestDATA*>*)arg;
+
+    DWORD id = GetCurrentThreadId();
+    int num = 0;
+    while (1)
+    {
+
+        for (int i = 0; i < 500; i++)
+        {
+            TestDATA* data;
+            bool res = lfQ->Dequeue(&data);
+            if (res == false)
+            {
+                //DebugBreak();
+            }
+            else
+            {
+                if (data->count != 1)
+                {
+                    DebugBreak();
+                }
+                delete data;
+            }
+
+        }
+    }
+    return 0;
+}
+
+unsigned __stdcall WorkerThread2(LPVOID arg)
+{
+    LFQueue<TestDATA*>* lfQ2 = (LFQueue<TestDATA*>*)arg;
+
+    DWORD id = GetCurrentThreadId();
+    int num = 0;
+    while (1)
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            TestDATA* data = new TestDATA;
+            data->data = num++;
+            data->count = 2;
+            lfQ2->Enqueue(data);
+        }
+        Sleep(1);
+    }
+    return 0;
+}
+
+unsigned __stdcall WorkerThread3(LPVOID arg)
+{
+    LFQueue<TestDATA*>* lfQ2 = (LFQueue<TestDATA*>*)arg;
+
+    DWORD id = GetCurrentThreadId();
+    int num = 0;
+    while (1)
+    {
+
+        for (int i = 0; i < 500; i++)
+        {
+            TestDATA* data;
+            bool res = lfQ2->Dequeue(&data);
+            if (res == false)
+            {
+                //DebugBreak();
+            }
+            else
+            {
+                if (data->count != 2)
+                {
+                    DebugBreak();
+                }
+                delete data;
+            }
+        }
+    }
+    return 0;
+}
+
+unsigned __stdcall WorkerThread4(LPVOID arg)
+{
+    LFQueue<TestDATA*>* lfQ3 = (LFQueue<TestDATA*>*)arg;
+    DWORD id = GetCurrentThreadId();
+    int num = 0;
+    while (1)
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            TestDATA* data = new TestDATA;
+            data->data = num++;
+            data->count = 3;
+            lfQ3->Enqueue(data);
+        }
+        Sleep(1);
+    }
+    return 0;
+}
+
+unsigned __stdcall WorkerThread5(LPVOID arg)
+{
+    LFQueue<TestDATA*>* lfQ3 = (LFQueue<TestDATA*>*)arg;
+
+    DWORD id = GetCurrentThreadId();
+    int num = 0;
+    while (1)
+    {
+
+        for (int i = 0; i < 500; i++)
+        {
+            TestDATA* data;
+            bool res = lfQ3->Dequeue(&data);
+            if (res == false)
+            {
+                //DebugBreak();
+            }
+            else
+            {
+                if (data->count != 3)
+                {
+                    DebugBreak();
+                }
+                delete data;
+            }
+        }
+    }
+    return 0;
+}
